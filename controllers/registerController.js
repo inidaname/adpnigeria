55090fe3f99d4cb132ec1e3c07fcf3035e78254e
Hassan Sani inidaname <saniyhassan@gmail.com>
2018-02-15 17:38:35 +0100

    All Sync

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -65,7 +65,7 @@ function isAuthenticated(req, res, next) {
       return next();
 
   // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
-  res.status(301).redirect('https://dashboard.adp.ng');
+  res.redirect('https://dashboard.adp.ng');
 }
 
 
